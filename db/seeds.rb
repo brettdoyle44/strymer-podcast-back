f2e128405afa274115bf3f8d5e010a6c2a44085c
brettdoyle44 <brettdoyle44@gmail.com>
2019-07-22 14:15:35 -0400

    Populate tables and test
    Populated tables with initial CSV seed data. Made sure relationships
    exist.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+Podcast.transaction do
+  CSV.foreach(Rails.root + 'data/podcast.csv',
+              headers: true,
+              header_converters: ->(h) { h.downcase.to_sym }) do |podcast_row|
+    podcast = podcast_row.to_hash
+    Podcast.create podcast unless Podcast.exists? podcast
+  end
+end
+
+Episode.transaction do
+  CSV.foreach(Rails.root + 'data/episode.csv',
+              headers: true,
+              header_converters: ->(h) { h.downcase.to_sym }) do |episode_row|
+    episode = episode_row.to_hash
+    Episode.create episode unless Episode.exists? episode
+  end
+end
