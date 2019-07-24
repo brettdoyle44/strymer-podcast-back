420f5a0734e0b1f3509fca1b46e700866099f9da
brettdoyle44 <brettdoyle44@gmail.com>
2019-07-24 17:18:15 -0400

    Created a destroy relationship for playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  has_many :playlist_podcasts
+  has_many :playlist_podcasts, dependent: :destroy
   has_many :podcasts, through: :playlist_podcasts
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -1,5 +1,5 @@
 class Podcast < ApplicationRecord
   has_many :episodes
-  has_many :playlist_podcasts
+  has_many :playlist_podcasts, dependent: :destroy
   has_many :playlists, through: :playlist_podcasts
 end
